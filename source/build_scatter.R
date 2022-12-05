d92321b322db98e01ed2f38c030d0c133d7a2273
Rin Le <rinle@Rins-MacBook-Pro.local>
2022-12-05 06:35:36 -0800

    Revised labels

--- source/build_scatter.R
+++ source/build_scatter.R
@@ -8,7 +8,11 @@ build_scatter <- function(data, input) {
     plot <- ggplot(data, aes(x = State, y = Total_Offenses, col = State)) +
       geom_point() +
       coord_cartesian(xlim = c(0, 50),
-                      ylim = c(0, 1640000))
+                      ylim = c(0, 1640000)) +
+      scale_y_continuous(limits = c(0, max(data$Total_Offenses))) +
+      theme(axis.text.x = element_blank()) + 
+      labs(title = paste("Total Number of Offenses in Each State"),
+           x = "State", y = "Total Number of Offenses")
     return(plot)
   } else {
     data <- data %>% 
@@ -16,7 +20,13 @@ build_scatter <- function(data, input) {
     plot <- ggplot(data = data, aes(x = State, y = Total_Offenses, col = State)) +
       geom_point() +
       coord_cartesian(xlim = c(0, 50),
-                      ylim = c(0, 1640000))
+                      ylim = c(0, 1640000)) +
+      scale_y_continuous(limits = c(0, max(data$Total_Offenses))) +
+      theme(axis.text.x = element_blank(),
+            legend.position="none"
+            ) + 
+      labs(title = paste("Total Number of Offenses in", input, sep = " "),
+           x = "State", y = "Total Number of Offenses")
     return(plot)
   }
 }
