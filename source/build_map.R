ea67e7edb7a9c39cb6f9155d6c4a10ca7d0ddc56
jessaykym <jkim1224@uw.edu>
2022-12-05 11:29:25 -0800

    m

--- source/build_map.R
+++ source/build_map.R
@@ -1,17 +1,22 @@
 # BuildMap file: function that returns a plotly map
 library(plotly)
-library(stringr)
 library(ggplot2)
 library(maps)
+library(dplyr)
+library(tidyverse)
 
 # BuildMap function: fill this in with a function that returns a map:
 
 build_map <- function(data, input) {
+  map_data <- map_data("state")
+  colnames(data) <- c("region", "Video.Games")
+  data$region <- tolower(data$region)
+  map_data <- left_join(map_data, data, by = "region")
   if(input != "All States") {
-    data <- data %>% 
-      filter(State == input)
+    map_data <- map_data %>% 
+      filter("region" == input)
   }
-  plot <- ggplot(data, aes(x = long, y = lat, group=group)) +
+  plot <- ggplot(map_data, aes(x = long, y = lat, group=group)) +
     geom_polygon(aes(fill = Video.Games), color = "black")
   return(plot)
 }
